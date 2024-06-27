9ac9e21f7887613a7d6d165993f9ea4d0a2fd786
Henrique Oliveira Gobi <oliveiragobihenrique@gmail.com>
2024-06-27 12:47:50 -0300

    refactor: test end2end

--- test/specs/orderTest.js
+++ test/specs/orderTest.js
@@ -1,75 +1,42 @@
 describe("Order Submission Test", () => {
+  // test
   it("should successfully submit an order and show the thank you page", async () => {
-    // search on api
+    // navigate to checkout page
     await browser.url("http://localhost:8080/CheckoutPage");
-
-    // wait page load
-    await browser.pause(5000);
-
-    // search field Nome completo
-    const nomeCompletoField = await $('[aria-label="Nome Completo"]');
-    await nomeCompletoField.waitForExist({ timeout: 5000 });
-
-    // fill the fields
-    await nomeCompletoField.setValue("Henrique Gobi");
-
-    const emailField = await $('[aria-label="Email"]');
-    await emailField.setValue("henrique.gobi@example.com");
-
-    const phoneField = await $('[aria-label="Telefone"]');
-    await phoneField.setValue("(99) 99999-9999");
-
-    const cepField = await $('[aria-label="CEP"]');
-    await cepField.setValue("53427650");
-    const body = await $("body");
-    await body.click(); //body click for fill endrees field
-    await browser.pause(2000);
-
-    const paymentMethodField = await $('[aria-label="Método de Pagamento"]');
-    await paymentMethodField.scrollIntoView();
-    await paymentMethodField.waitForExist({ timeout: 5000 });
-
-    await browser.execute(function () {
-      const field = document.querySelector(
-        '[aria-label="Método de Pagamento"]'
-      );
-      field.scrollIntoView();
+    // wait until page is fully loaded
+    await browser.waitUntil(async () => await $("body").isDisplayed(), {
+      timeout: 10000,
+      timeoutMsg: "Expected body to be displayed after loading",
     });
+    // fill fields
+    await $('[aria-label="Nome Completo"]').setValue("Henrique Gobi");
+    await $('[aria-label="Email"]').setValue("henrique.gobi@example.com");
+    await $('[aria-label="Telefone"]').setValue("(99) 99999-9999");
+    await $('[aria-label="CEP"]').setValue("53427650");
+    // wait for address to be filled
+    await browser.keys("Tab");
+    await browser.pause(3000);
 
-    await paymentMethodField.click();
+    // Dont work
+    await $('[aria-label="Método de Pagamento"]').click();
+    await $("=Cartão de Crédito").waitForDisplayed({ timeout: 5000 });
+    await $("=Cartão de Crédito").click();
 
-    const opcaoCartaoCredito = await $(
-      '//div[contains(text(),"Cartão de Crédito")]'
-    );
-    await opcaoCartaoCredito.waitForExist({ timeout: 5000 });
-
-    await opcaoCartaoCredito.click();
-
-    const cardNumberField = await $('[aria-label="Número do Cartão"]');
-    await cardNumberField.waitForExist({ timeout: 5000 });
-    await cardNumberField.setValue("4111 1111 1111 1111");
-
-    const cardCvvField = await $('[aria-label="CVV"]');
-    await cardCvvField.setValue("123");
-
-    const cardExpiryField = await $('[aria-label="Data de Validade (MM/AA)"]');
-    await cardExpiryField.setValue("12/24");
-
-    const cpfField = await $('[aria-label="CPF"]');
-    await cpfField.setValue("12345678900");
-
-    const submitButton = await $('button[type="submit"]');
-    await submitButton.waitForExist({ timeout: 10000 });
-    await submitButton.click();
-
-    //show details
-    const thankYouHeader = await $("h3");
-    await thankYouHeader.waitForExist({ timeout: 10000 });
-    await expect(thankYouHeader).toBeDisplayed();
-    await expect(thankYouHeader).toHaveText("Detalhes do Pedido");
-    const orderStatus = await $("p");
-    await orderStatus.waitForExist({ timeout: 10000 });
-    await expect(orderStatus).toHaveTextContaining(
+    // set number of card
+    await $('[aria-label="Número do Cartão"]').setValue("4111111111111111");
+    // set cvv
+    await $('[aria-label="CVV"]').setValue("123");
+    // set expiration date
+    await $('[aria-label="Data de Validade (MM/AA)"]').setValue("12/28");
+    // set cpf
+    await $('[aria-label="CPF"]').setValue("12345678900");
+    // submit form
+    await $('button[type="submit"]').click();
+    // wait for thank you page to be displayed
+    await $("h3").waitForDisplayed({ timeout: 10000 });
+    // check details of the order
+    expect(await $("h3").getText()).toContain("Detalhes do Pedido");
+    expect(await $("p").getText()).toContain(
       "Status do Pedido: Pagamento realizado"
     );
   });
